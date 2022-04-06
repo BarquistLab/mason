851cb23272b9bcd2fa07488416b6f2f22d5d43ea
Jakob Jung <jakobjung@tutanota.com>
2022-04-06 08:09:47 +0200

    trial mailing of results

--- browser/pnag/__init__.py
+++ browser/pnag/__init__.py
@@ -19,9 +19,10 @@ app.config['SECRET_KEY'] = config.get('SECRET_KEY')
 
 bcrypt = Bcrypt(app)
 # to sent the reset password emails
-app.config['MAIL_SERVER'] = 'mail.gmx.net'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = "smtp.gmail.com"  #'smtp.web.de'
+app.config['MAIL_PORT'] = 587  # 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 # both need to be set before installation
 app.config['MAIL_USERNAME'] = config.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = config.get('EMAIL_PASS')
